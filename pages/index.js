20ee0c3afb9f32a51243a1b347106bf37b58c1d4
mama900ai-wq <mama900ai@gmail.com>
2026-06-29 11:42:01 +0200

    Add pages/index.js - Home page with GamingStore component

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import { ShoppingCart, Phone, X, Plus, Minus } from 'lucide-react';
+import GamingStore from '../gaming_store';
+
+export default function Home() {
+  return <GamingStore />;
+}
