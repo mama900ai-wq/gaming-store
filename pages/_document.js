b2480664013df6c6f681a00596979135eaed4ff9
mama900ai-wq <mama900ai@gmail.com>
2026-06-29 12:28:40 +0200

    Fix pages/_document.js - Proper Next.js document structure

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,24 @@
+import React from 'react';
 import Head from 'next/head';
+import { Html, Head as NextHead, Main, NextScript } from 'next/document';
 
-export default function MyDocument() {
+export default function Document() {
   return (
-    <>
-      <Head>
+    <Html lang="ar" dir="rtl">
+      <NextHead>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="متجر الفردوس الإلكتروني - شحن فوري لأفضل الألعاب" />
         <meta name="keywords" content="gaming, store, games, top-up, فري فاير, كلاش, إيفوتبول" />
-        <title>متجر الفردوس الإلكتروني - Gaming Store</title>
+        <meta name="theme-color" content="#0f172a" />
         <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      </NextHead>
+      <body style={{ backgroundColor: '#0f172a', color: '#fff' }}>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
   );
 }
