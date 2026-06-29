b07684e54960b7a09821bbbde06c18d4f8fc0ae2
mama900ai-wq <mama900ai@gmail.com>
2026-06-29 12:24:14 +0200

    Add next.config.js - Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compress: true,
+  productionBrowserSourceMaps: false,
+  optimizeFonts: true,
+};
+
+module.exports = nextConfig;
