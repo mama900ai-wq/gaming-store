c501fb3e1f8120900865cf35a5e35595d8961bc4
mama900ai-wq <mama900ai@gmail.com>
2026-06-29 11:42:16 +0200

    Add pages/_document.js - Next.js document component

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function MyDocument() {
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="متجر الفردوس الإلكتروني - شحن فوري لأفضل الألعاب" />
+        <meta name="keywords" content="gaming, store, games, top-up, فري فاير, كلاش, إيفوتبول" />
+        <title>متجر الفردوس الإلكتروني - Gaming Store</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+}
